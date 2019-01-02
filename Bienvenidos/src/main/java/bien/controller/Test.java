431ad592e79d4e619789544dd08db2c7c8eabd2d
Youngeun <young-eun94@hanmail.net>
2019-01-02 21:46:39 +0900

    190102 cssLoader commit

---
+++ Bienvenidos/src/main/java/bien/controller/Test.java
@@ -0,0 +1,16 @@
+package bien.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/list")
+public class Test {
+	
+	@RequestMapping(value="/includeT", method=RequestMethod.GET)
+	public void test1() {
+		
+	}
+
+}
---
+++ Bienvenidos/src/main/webapp/WEB-INF/views/list/includeT.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ include file="../include/cssLoader.jsp" %>
+<%@ include file="../include/header.jsp" %>
+
+<div>
+	<img style="min-width: 100%;" src="/resources/image/mainPic.png">
+</div>
+
+</body>
+</html>
